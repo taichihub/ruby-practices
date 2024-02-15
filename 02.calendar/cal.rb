20bbe8ac56d26e10111b938eda61a22979dc7bfb
taichihub <taichikuniv21@gmail.com>
2024-02-15 20:07:55 +0900

    カレンダー修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'date' # Dateクラスを使うため
-require 'optparse' # コマンドライン引数をパースするため
+require 'date' 
+require 'optparse' 
 
-# コマンドライン引数をパース
 def parse_arguments
   options = {}
   OptionParser.new do |opts|
@@ -13,38 +12,42 @@ def parse_arguments
     opts.on("-m MONTH") do |month|
       options[:month] = month.to_i
     end
-  end.parse! # ARGV削除
-  options # ハッシュで返す
+  end.parse! 
+  options 
 end
 
-# カレンダー表示
 def print_calendar(year, month)
-  first_day = Date.new(year, month, 1)
-  last_day = Date.new(year, month, -1)
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
 
-  puts first_day.strftime("%-m月 %Y").center(20)
+  puts first_date.strftime("%-m月 %Y").center(20)
   puts "日 月 火 水 木 金 土"
-  print "   " * first_day.wday # 1日までの空白を出力
+  print "   " * first_date.wday 
 
-  (first_day..last_day).each do |date|
-    print date.day.to_s.rjust(2) + " "
-    print "\n" if date.saturday? # 土曜日の場合は改行
+  (first_date..last_date).each do |date|
+    if date.saturday?
+      print date.day.to_s.rjust(2) + "\n"
+    else
+      print date.day.to_s.rjust(2) + " "
+    end
   end
-  print "\n" unless last_day.saturday? # 最終日が土曜日じゃない場合は改行
+  print "\n" unless last_date.saturday?
 end
 
-# メイン処理
+def valid_date?(year, month)
+  year.is_a?(Integer) && year > 0 && month.is_a?(Integer) && month.between?(1, 12)
+end
+
 def main
   options = parse_arguments
   year = options[:year] || Date.today.year
   month = options[:month] || Date.today.month
 
-  begin
+  if valid_date?(year, month)
     print_calendar(year, month)
-  rescue ArgumentError
+  else
     puts "無効な日付が指定されました"
   end
 end
 
-# コマンドラインから実行された場合のみ動作
 main if __FILE__ == $0
