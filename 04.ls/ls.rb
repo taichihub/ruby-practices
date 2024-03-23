e5ce7dafd1b4cb6d1d0096d0819e828f5a634393
taichihub <taichikuniv21@gmail.com>
2024-03-23 16:39:01 +0900

    lsコマンド実装レビュー修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,30 +1,18 @@
 # frozen_string_literal: true
 
 COLUMNS = 3
-
-SPACES = 8
-
-class Array
-  def sefe_transpose
-    max_length = map(&:length).max
-    map do |row|
-      row.dup.fill(nil, row.length...max_length)
-    end.transpose
-  end
-end
+BLANK_SIZE = 8
 
 def fetch_entries
   Dir.glob('*')
 end
 
-def natural_sort(files)
-  files.sort_by do |file|
-    file.split(/(\d+)/).map { |e| e.match?(/\d+/) ? e.to_i : e }
-  end
+def dictionary_sort(files)
+  files.sort
 end
 
-def calculate_items_per_column(total_items)
-  (total_items.to_f / COLUMNS).ceil
+def calculate_items_per_column(items)
+  (items.to_f / COLUMNS).ceil
 end
 
 def slice_entries_for_display(entries, items_per_column)
@@ -37,10 +25,18 @@ def calculate_max_widths(formatted_entries)
   end
 end
 
+def safe_transpose(entries)
+  max_length = entries.map(&:size).max
+  padded_entries = entries.map do |row|
+    row.dup.fill(nil, row.length...max_length)
+  end
+  padded_entries.transpose
+end
+
 def print_entries(formatted_entries, max_widths)
-  formatted_entries.sefe_transpose.each do |row|
+  safe_transpose(formatted_entries).each do |row|
     row.each_with_index do |entry, index|
-      print (entry || '').ljust(max_widths[index] + SPACES)
+      print (entry || '').ljust(max_widths[index] + BLANK_SIZE)
     end
     puts
   end
@@ -48,7 +44,7 @@ end
 
 def main
   entries = fetch_entries
-  sorted_entries = natural_sort(entries)
+  sorted_entries = dictionary_sort(entries)
   items_per_column = calculate_items_per_column(sorted_entries.size)
   formatted_entries = slice_entries_for_display(sorted_entries, items_per_column)
   max_widths = calculate_max_widths(formatted_entries)
