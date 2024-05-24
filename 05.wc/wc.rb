31b5ec05fe04bf510e5311fbc6d294cb02c7fd99
taichihub <taichikuniv21@gmail.com>
2024-05-24 15:35:44 +0900

    rubocopで指摘された部分を修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'optparse'
 
@@ -9,7 +9,7 @@ end
 
 # 単語数をカウントする関数（1文字=1単語）
 def count_words(content)
-  content.each_char.reject { |char| char =~ /\s/ }.count
+  content.each_char.count { |char| char =~ /\S/ }
 end
 
 # バイト数をカウントする関数
@@ -20,25 +20,15 @@ end
 # オプションの解析
 options = { lines: false, words: false, bytes: false }
 OptionParser.new do |opts|
-  opts.banner = "Usage: wc.rb [options] [file]"
+  opts.banner = 'Usage: wc.rb [options] [file]'
 
-  opts.on("-l", "Count lines") do |l|
-    options[:lines] = true
-  end
-
-  opts.on("-w", "Count words") do |w|
-    options[:words] = true
-  end
-
-  opts.on("-c", "Count bytes") do |c|
-    options[:bytes] = true
-  end
+  opts.on('-l', 'Count lines') { options[:lines] = true }
+  opts.on('-w', 'Count words') { options[:words] = true }
+  opts.on('-c', 'Count bytes') { options[:bytes] = true }
 end.parse!
 
 # オプションが指定されていない場合、すべてのオプションを有効にする
-if !options[:lines] && !options[:words] && !options[:bytes]
-  options[:lines] = options[:words] = options[:bytes] = true
-end
+options[:lines] = options[:words] = options[:bytes] = true unless options.values.any?
 
 # ファイルを読み込み
 file_content = ARGF.read
@@ -55,4 +45,4 @@ output << word_count if options[:words]
 output << byte_count if options[:bytes]
 
 # 出力をスペース区切りで表示
-puts output.join(" ")
+puts output.join(' ')
