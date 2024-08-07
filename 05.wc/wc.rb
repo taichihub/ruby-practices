fdbf8525c9fabe895d5a774cc2d61a2e9d6410c8
taichihub <taichikuniv21@gmail.com>
2024-08-08 08:36:55 +0900

    0808レビュー修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,7 +4,6 @@ require 'optparse'
 
 def main
   options = parse_options
-
   if ARGV.empty?
     handle_stdin(options)
   else
@@ -26,28 +25,23 @@ end
 def handle_stdin(options)
   file_content = $stdin.read
   counts = count_all(file_content)
-  max_widths = calculate_max_widths([counts])
-  output_results(counts, options, nil, max_widths)
+  output_results(counts, options)
 end
 
 def handle_files(files, options)
   file_counts = files.map { |file| count_file_contents(file) }
-  max_widths = calculate_max_widths(file_counts.map(&:first))
+  file_counts.each { |counts, filename| output_results(counts, options, filename) }
+  output_total(file_counts, options) if files.size > 1
+end
 
-  file_counts.each do |counts, filename|
-    output_results(counts, options, filename, max_widths)
-  end
-
-  return unless files.size > 1
-
+def output_total(file_counts, options)
   total_counts = calculate_totals(file_counts.map(&:first))
-  output_results(total_counts, options, 'total', max_widths)
+  output_results(total_counts, options, 'total')
 end
 
 def count_file_contents(file)
   file_content = File.read(file)
-  counts = count_all(file_content)
-  [counts, file]
+  [count_all(file_content), file]
 end
 
 def calculate_totals(file_counts)
@@ -56,13 +50,6 @@ def calculate_totals(file_counts)
     words: file_counts.sum { |counts| counts[:words] },
     bytes: file_counts.sum { |counts| counts[:bytes] }
   }
-end
-
-def calculate_max_widths(counts_list)
-  max_lines = counts_list.map { |counts| counts[:lines].to_s.size }.max
-  max_words = counts_list.map { |counts| counts[:words].to_s.size }.max
-  max_bytes = counts_list.map { |counts| counts[:bytes].to_s.size }.max
-  [max_lines, max_words, max_bytes].map { |w| [w, 6].max }
 end
 
 def count_all(content)
@@ -85,17 +72,70 @@ def count_bytes(content)
   content.bytesize
 end
 
-def output_results(counts, options, filename = nil, max_widths = [6, 6, 6])
-  output = []
-  output << format(" %#{max_widths[0]}d", counts[:lines]) if options[:lines] || !any_option_selected?(options)
-  output << format(" %#{max_widths[1]}d", counts[:words]) if options[:words] || !any_option_selected?(options)
-  output << format(" %#{max_widths[2]}d", counts[:bytes]) if options[:bytes] || !any_option_selected?(options)
+def output_results(counts, options, filename = nil)
+  output = build_output(counts, options)
+  output.unshift('') if prepend_space?(options)
   output << filename unless filename.nil?
   puts output.join(' ')
 end
 
+def build_output(counts, options)
+  [].tap do |output|
+    add_lines_to_output(output, counts, options)
+    add_words_to_output(output, counts, options)
+    add_bytes_to_output(output, counts, options)
+  end
+end
+
+def add_lines_to_output(output, counts, options)
+  output << format('%8d', counts[:lines]) if options[:lines] || !any_option_selected?(options)
+end
+
+def add_words_to_output(output, counts, options)
+  output << format_words(counts) if options[:words] || !any_option_selected?(options)
+end
+
+def add_bytes_to_output(output, counts, options)
+  output << format_bytes(counts) if options[:bytes] || !any_option_selected?(options)
+end
+
 def any_option_selected?(options)
   options[:lines] || options[:words] || options[:bytes]
+end
+
+def format_words(counts)
+  format("%#{8 - counts[:lines].to_s.size}d", counts[:words])
+end
+
+def format_bytes(counts)
+  case counts[:words].to_s.size
+  when 3
+    format_adjusted_bytes(10, counts)
+  when 2
+    format_adjusted_bytes(9, counts)
+  when 1
+    format_adjusted_bytes(8, counts)
+  end
+end
+
+def format_adjusted_bytes(number, counts)
+  format("%#{number - counts[:words].to_s.size}d", counts[:bytes])
+end
+
+def prepend_space?(options)
+  word_option?(options) || byte_option?(options) || word_and_byte_option?(options)
+end
+
+def word_option?(options)
+  options[:words] && !options[:lines] && !options[:bytes]
+end
+
+def byte_option?(options)
+  options[:bytes] && !options[:lines] && !options[:words]
+end
+
+def word_and_byte_option?(options)
+  options[:words] && options[:bytes] && !options[:lines]
 end
 
 main
