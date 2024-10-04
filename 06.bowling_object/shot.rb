1eaff6204a8db18409f50ab12873bcc60f1f82d8
taichihub <taichikuniv21@gmail.com>
2024-10-04 17:25:23 +0900

    ストライクマークのスコアを追加で定義した

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -4,8 +4,9 @@ class Shot
   attr_reader :pins
 
   STRIKE_MARK = 'X'
+  STRIKE_PINS = 10
 
   def initialize(score)
-    @pins = score == STRIKE_MARK ? MAX_PINS : score.to_i
+    @pins = score == STRIKE_MARK ? STRIKE_PINS : score.to_i
   end
 end
