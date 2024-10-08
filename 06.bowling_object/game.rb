9a0e7b21c3f1fcbcbe3e9818450454ce7e2482e5
taichihub <taichikuniv21@gmail.com>
2024-10-08 21:44:22 +0900

    game.rbのリファクタリング

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -6,9 +6,8 @@ class Game
   FRAMES = 10
 
   def initialize(pins)
-    @frames = []
     frames_data = split_pins_into_frames(pins)
-    frames_data.each_with_index.map { |frame_pins, index| @frames << Frame.new(frame_pins, index) }
+    @frames = frames_data.each_with_index.map { |frame_pins, index| Frame.new(frame_pins, index) }
   end
 
   def score
@@ -25,13 +24,15 @@ class Game
 
     pins.each do |pin|
       frame << pin
-      if frames.size != FRAMES - 1 && (frame.sum(&:pins) == MAX_PINS || frame.size == PER_FRAME_MAX_SHOTS)
+      next if frames.size == FRAMES - 1
+
+      if frame.sum(&:pins) == MAX_PINS || frame.size == PER_FRAME_MAX_SHOTS
         frames << frame
         frame = []
       end
     end
 
-    frames << frame unless frame.empty?
+    frames << frame if !frame.empty?
     frames
   end
 end
