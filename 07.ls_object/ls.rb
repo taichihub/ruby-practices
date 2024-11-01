dbfabdacaeea3b67264d485c8feaf1afebac4611
taichihub <taichikuniv21@gmail.com>
2024-11-01 19:40:52 +0900

    lsコマンドメインスクリプト

---
+++ 07.ls_object/ls.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'option_parser'
+
+require_relative 'directory_lister'
+
+if __FILE__ == $PROGRAM_NAME
+  options = OptionParser.new(ARGV)
+  DirectoryLister.new(options).list
+end
