c2fed7ec7a752fe67926da3d2e57abc0e8f793b3
taichihub <taichikuniv21@gmail.com>
2024-11-13 16:21:20 +0900

    権限表示ロジックを修正

--- 07.ls_object/file_entry.rb
+++ 07.ls_object/file_entry.rb
@@ -12,7 +12,7 @@ class FileEntry
 
   def detailed_info(size_width)
     ftype = file_type
-    permissions = format('%o', @stat.mode)[-3, 3].chars.map { |ch| ch.to_i.to_s(2).rjust(3, '0') }.join.tr('1', 'r').tr('0', '-')
+    permissions = format_permissions
     nlink = @stat.nlink
     owner = Etc.getpwuid(@stat.uid).name
     group = Etc.getgrgid(@stat.gid).name
@@ -22,6 +22,15 @@ class FileEntry
   end
 
   private
+
+  def format_permissions
+    modes = {
+      0 => "---", 1 => "--x", 2 => "-w-", 3 => "-wx",
+      4 => "r--", 5 => "r-x", 6 => "rw-", 7 => "rwx"
+    }
+    octal_mode = format("%o", @stat.mode)[-3, 3]
+    octal_mode.chars.map { |n| modes[n.to_i] }.join
+  end  
 
   def file_type
     case @stat.ftype
