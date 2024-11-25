3def0b59cfa415a7b6ecadd76724d29098d68f33
taichihub <taichikuniv21@gmail.com>
2024-11-25 12:53:33 +0900

    スクリプト直接実行の確認分岐を削除

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -3,7 +3,5 @@
 require_relative 'parsed_options'
 require_relative 'directory_lister'
 
-if __FILE__ == $PROGRAM_NAME
-  parsed_options = ParsedOptions.new(ARGV)
-  DirectoryLister.new(parsed_options).list
-end
+parsed_options = ParsedOptions.new(ARGV)
+DirectoryLister.new(parsed_options).list
